69a3e500f1fa3dfcc2217db2536a29e6df1df9db
Lalo Rivero <eduardo.rivero@cobachih.edu.mx>
2020-03-01 14:38:46 -0700

    v-for simple / objetos

--- 01-BasicoVue_nachoAnaya/curso-basico-vuejs-master/app.js
+++ 01-BasicoVue_nachoAnaya/curso-basico-vuejs-master/app.js
@@ -6,6 +6,15 @@ var app = new Vue({
       img:'https://cryptologos.cc/logos/bitcoin-btc-logo.png',
       changePercent:10,
       prices: [8400,7900,8200,9000,9400,8700,8374],
+      pricesWithDays:[
+        {day:"Monday", value: 8400},
+        {day:"Tuesday", value: 7900},
+        {day:"Wednesday", value: 8800},
+        {day:"Thursday", value: 9000},
+        {day:"Friday", value: 9500},
+        {day:"Saturday", value: 10000},
+        {day:"Sunday", value: 10400},
+      ]
     }
   }
 })
