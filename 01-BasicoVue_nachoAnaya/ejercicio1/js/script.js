3901a9cfceb9652cf086fbb37d18f1b0361b4f60
Lalo Rivero <eduardo.rivero@cobachih.edu.mx>
2020-03-03 20:47:23 -0700

    Primer ejercicio Vue basico

--- 01-BasicoVue_nachoAnaya/ejercicio1/js/script.js
+++ 01-BasicoVue_nachoAnaya/ejercicio1/js/script.js
@@ -1,14 +1,26 @@
-var vue = new Vue({
+var app = new Vue({
   el : "#app",
   data(){
     return {
-
+      courses :[],
+      title: '',
+      time: '',
     }
   },
   computed:{
-
+    totalTime(){
+      const toNumber = this.courses.map(c => parseInt(c.time))
+      const suma = toNumber.reduce((a,b) => a+b, 0);
+      return suma;
+    }
   },
   methods:{
-
+    addCourse() {
+      var newCourse = this.courses.push({
+        title: this.title,
+        time: this.time,
+      });
+      return this.courses;
+    }
   }
 })
