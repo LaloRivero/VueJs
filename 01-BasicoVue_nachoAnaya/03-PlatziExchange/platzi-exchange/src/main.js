fc43df3ad6035603965b92f7011f84bb4431f15c
Lalo Rivero <eduardo.rivero@cobachih.edu.mx>
2020-03-08 22:11:23 -0700

    filtros

--- 01-BasicoVue_nachoAnaya/03-PlatziExchange/platzi-exchange/src/main.js
+++ 01-BasicoVue_nachoAnaya/03-PlatziExchange/platzi-exchange/src/main.js
@@ -1,11 +1,15 @@
-import Vue from "vue";
-import App from "./App.vue";
-import "@/assets/css/tailwind.css";
-import router from "@/router";
+import Vue from 'vue'
+import App from './App.vue'
+import '@/assets/css/tailwind.css'
+import router from '@/router'
+import { dollarFilter } from '@/filters'
+import { percentFilter } from '@/filters'
 
-Vue.config.productionTip = false;
+Vue.filter('dollar', dollarFilter)
+Vue.filter('percent', percentFilter)
+Vue.config.productionTip = false
 
 new Vue({
   router,
   render: h => h(App)
-}).$mount("#app");
+}).$mount('#app')
