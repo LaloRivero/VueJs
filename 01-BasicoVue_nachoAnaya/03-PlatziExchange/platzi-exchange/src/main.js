098b25e050076460b23bbb7638ab67bfd6fad58e
Lalo Rivero <eduardo.rivero@cobachih.edu.mx>
2020-03-09 14:11:58 -0700

    proyecto terminado

--- 01-BasicoVue_nachoAnaya/03-PlatziExchange/platzi-exchange/src/main.js
+++ 01-BasicoVue_nachoAnaya/03-PlatziExchange/platzi-exchange/src/main.js
@@ -1,5 +1,8 @@
 import Vue from 'vue'
 import App from './App.vue'
+import Chart from 'chart.js'
+import Chartick from 'vue-chartkick'
+import { VueSpinners } from '@saeris/vue-spinners'
 import '@/assets/css/tailwind.css'
 import router from '@/router'
 import { dollarFilter } from '@/filters'
@@ -8,6 +11,8 @@ import { percentFilter } from '@/filters'
 Vue.filter('dollar', dollarFilter)
 Vue.filter('percent', percentFilter)
 Vue.config.productionTip = false
+Vue.use(VueSpinners)
+Vue.use(Chartick.use(Chart))
 
 new Vue({
   router,
--- 01-BasicoVue_nachoAnaya/03-PlatziExchange/platzi-exchange/src/views/CoinDetail.vue
+++ 01-BasicoVue_nachoAnaya/03-PlatziExchange/platzi-exchange/src/views/CoinDetail.vue
@@ -1,6 +1,9 @@
 <template>
   <div class="flex-col">
-    <template v-if="asset.id">
+    <div class="flex justify-center">
+      <hash-loader :loading="isLoading" :color="'#68d391'" :size="100" />
+    </div>
+    <template v-if="!isLoading">
       <div class="flex flex-col sm:flex-row justify-around items-center">
         <div class="flex flex-col items-center">
           <img
@@ -47,42 +50,100 @@
 
         <div class="my-10 sm:mt-0 flex flex-col justify-center text-center">
           <button
+            @click="toggleConverter"
             class="bg-green-500 hover:bg-green-700 text-white font-bold py-2 px-4 rounded"
           >
-            Cambiar
+            {{ fromUsd ? `USD a ${asset.symbol}` : `${asset.symbol} a USD` }}
           </button>
 
           <div class="flex flex-row my-5">
             <label class="w-full" for="convertValue">
               <input
+                v-model="convertValue"
                 id="convertValue"
                 type="number"
                 class="text-center bg-white focus:outline-none focus:shadow-outline border border-gray-300 rounded-lg py-2 px-4 block w-full appearance-none leading-normal"
+                :placeholder="`Valor en ${fromUsd ? 'USD' : asset.symbol}`"
               />
             </label>
           </div>
 
-          <span class="text-xl"></span>
+          <span class="text-xl">
+            {{ convertResult }} {{ fromUsd ? asset.symbol : ` a USD` }}
+          </span>
         </div>
       </div>
+      <line-chart
+        class="my-10"
+        :colors="['orange']"
+        :min="min"
+        :max="max"
+        :data="history.map(h => [h.date, parseFloat(h.priceUsd).toFixed(2)])"
+      />
+      <h3 class="text-xl my-10">Mejores Ofertas de Cambio</h3>
+      <table style="width: 100%">
+        <tr
+          v-for="m in markets"
+          :key="`${m.exchangeId}-${m.priceUsd}`"
+          class="border-b"
+        >
+          <td>
+            <b>{{ m.exchangeId }}</b>
+          </td>
+          <td>{{ m.priceUsd | dollar }}</td>
+          <td>{{ m.baseSymbol }} / {{ m.quoteSymbol }}</td>
+          <td>
+            <px-button
+              :is-loading="m.isLoading || false"
+              v-if="!m.url"
+              @custom-click="getWebSite(m)"
+            >
+              <slot>Obtener Link</slot>
+            </px-button>
+
+            <a v-else class="hover:underline text-green-600" target="_blanck">{{
+              m.url
+            }}</a>
+          </td>
+        </tr>
+      </table>
     </template>
   </div>
 </template>
 
 <script>
+import PxButton from '@/components/PxButton'
 import api from '@/views/api'
 export default {
   name: 'CoinDetail',
   data() {
     return {
+      isLoading: false,
       asset: {},
-      history: []
+      history: [],
+      markets: [],
+      exchange: [],
+      fromUsd: true,
+      convertValue: null
     }
+  },
+  components: {
+    PxButton
   },
   created() {
     this.getCoin()
   },
   computed: {
+    convertResult() {
+      if (!this.convertValue) {
+        return 0
+      }
+      const result = this.fromUsd
+        ? this.convertValue / this.asset.priceUsd
+        : this.convertValue * this.asset.priceUsd
+
+      return result.toFixed(4)
+    },
     min() {
       return Math.min(
         ...this.history.map(h => parseFloat(h.priceUsd).toFixed(2))
@@ -99,16 +160,51 @@ export default {
       )
     }
   },
+  watch: {
+    $route() {
+      this.getCoin()
+    }
+  },
   methods: {
+    toggleConverter() {
+      this.fromUsd = !this.fromUsd
+    },
+    getWebSite(exchange) {
+      this.$set(exchange, 'isLoagind', true)
+
+      return api
+        .getExchange(exchange.exchangeId)
+        .then(res => {
+          this.$set(exchange, 'url', res.exchangeUrl)
+        })
+        .finally(() => {
+          this.$set(exchange, 'isLoagind', false)
+        })
+    },
     getCoin() {
       const id = this.$route.params.id
-      Promise.all([api.getAsset(id), api.getAssetHistory(id)]).then(
-        ([asset, history]) => {
+      this.isLoading = true
+      Promise.all([
+        api.getAsset(id),
+        api.getAssetHistory(id),
+        api.getMarkets(id),
+        api.getExchange(id)
+      ])
+        .then(([asset, history, market, exchange]) => {
           this.asset = asset
           this.history = history
-        }
-      )
+          this.markets = market
+          this.exchange = exchange
+        })
+        .finally(() => (this.isLoading = false))
     }
   }
 }
 </script>
+
+<style scoped>
+td {
+  padding: 10px;
+  text-align: center;
+}
+</style>
