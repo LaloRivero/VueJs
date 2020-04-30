bed8bed8325cef746225bf5ee9bf4623db52b514
Eduardo Rivero <eduardo.rivero@cobachih.edu.mx>
2020-04-30 14:15:51 -0600

    Curso Avanzado de Vuejs con Bedu

--- 03-Avanzado-Bedu/platzi-rooms/src/main.js
+++ 03-Avanzado-Bedu/platzi-rooms/src/main.js
@@ -1,21 +1,12 @@
 import Vue from 'vue'
 import firebase from 'firebase'
+import APIkey from './APIkey'
 import App from './App.vue'
 import router from './router'
 import store from './store'
 
 Vue.config.productionTip = false
-
-const firebaseConfig = {
-  apiKey: 'AIzaSyAvFMPTCFcAi7TjfRw9ojhD2dSj2KIYjjY',
-  authDomain: 'platzi-rooms-254cf.firebaseapp.com',
-  databaseURL: 'https://platzi-rooms-254cf.firebaseio.com',
-  projectId: 'platzi-rooms-254cf',
-  storageBucket: 'platzi-rooms-254cf.appspot.com',
-  messagingSenderId: '1063696170261',
-  appId: '1:1063696170261:web:737d32d2624687ef0e3d1c',
-  measurementId: 'G-74XB2VZEJB',
-}
+const firebaseConfig = APIkey
 // Initialize Firebase
 firebase.initializeApp(firebaseConfig)
 firebase.auth().onAuthStateChanged((user) => {
